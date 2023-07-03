ab93fb850bcdc46cf9d560ff38d0c6edfe393538
devid-corsard <truegrinde@gmail.com>
2023-07-03 14:34:30 +0300

    deploy: fixed scripts;

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -12,6 +12,7 @@ sudo chmod -R 777 /home/ubuntu/rustapps/newsletter_cd
 cd /home/ubuntu/rustapps/newsletter_cd 
 sqlx --database-url "$(cat ../newsletter_dburl)" database create
 sqlx --database-url "$(cat ../newsletter_dburl)" migrate run
+export SQLX_OFFLINE=true
 cargo build --release --bin cloud_app
 mv target/release/cloud_app cloud_app
 
